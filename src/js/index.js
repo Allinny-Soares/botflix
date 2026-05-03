7e8bbbd92fc9da828669f887ff79f0adff25bfd6
Allinny-Soares <allinnyjannesoaressilva@gmail.com>
2026-05-03 17:25:27 -0300

    Event Listeners

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,9 @@ document.addEventListener('DOMContentLoaded', function () {
     setupEventListeners();
     updateSearchButton();
 });
+
+function setupEventListeners() {
+    // Input change listener
+    moodInput.addEventListener('input', function () {
+        updateSearchButton();
+    });
