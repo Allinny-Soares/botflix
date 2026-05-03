9812d2f148237ad48891bd402fd431b7a954393a
Allinny-Soares <allinnyjannesoaressilva@gmail.com>
2026-05-03 17:11:50 -0300

    initialize app

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,7 @@
 const moodInput = document.getElementById('mood-input');
 const searchButton = document.getElementById('search-button');
+
+document.addEventListener('DOMContentLoaded', function () {
+    setupEventListeners();
+    updateSearchButton();
+});
